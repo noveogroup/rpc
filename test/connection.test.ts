d8e97ccb75debd55a67f7384f7d4e892a5657e38
Andrew D.Laptev <a.d.laptev@gmail.com>
2020-04-14 04:20:54 +0300

    Tests

--- test/connection.test.ts
+++ test/connection.test.ts
@@ -6,22 +6,59 @@ let server!: Server;
 beforeAll(() => {
   server = new Server({
     port: 3666,
+    handshake: (token) => {
+      return !token.startsWith('bad');
+    },
   });
 });
 
 afterAll(() => {
-  return server.close();
+  server.close();
 });
 
-test('First connection emits events', async () => {
+test('First connection emits `connect` event', async () => {
   const client = new ReconnectingClient({
     address: 'ws://localhost:3666',
-    token: 'test-client',
+    token: 'good-client-1',
   });
 
-  return new Promise((resolve, reject) => {
+  await new Promise((resolve, reject) => {
     client.addEventListener('connect', resolve);
     client.addEventListener('connectError', reject);
     client.init();
   });
+});
+
+test('First connection emits `connectError` event', async () => {
+  const client = new ReconnectingClient({
+    address: 'ws://localhost:3666',
+    token: 'bad-client-1',
+  });
+
+  await new Promise((resolve, reject) => {
+    client.addEventListener('connectError', resolve);
+    client.addEventListener('connect', reject);
+    client.init().catch(() => {}); // It must throws
+  });
+});
+
+test('First connection with promise resolves', async () => {
+  const client = new ReconnectingClient({
+    address: 'ws://localhost:3666',
+    token: 'good-client-2',
+  });
+  try {
+    await client.init();
+  } catch (e) {
+    console.log(e);
+    console.log(e.stack);
+  }
+});
+
+test('First connection with promise resolves', async () => {
+  const client = new ReconnectingClient({
+    address: 'ws://localhost:3666',
+    token: 'bad-client-2',
+  });
+  await expect(client.init()).rejects;
 });
